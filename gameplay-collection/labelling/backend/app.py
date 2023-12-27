bbcc290a9981c02502872c16514e72edf249732e
Shane Williams <46091293+shanethomaswilliams@users.noreply.github.com>
2023-12-27 11:59:13 -0800

    :construction: real time sending error

--- gameplay-collection/labelling/backend/app.py
+++ gameplay-collection/labelling/backend/app.py
@@ -1,3 +1,4 @@
+import asyncio
 from flask import Flask, request,jsonify
 from flask_socketio import SocketIO,emit
 from flask_cors import CORS
@@ -26,35 +27,63 @@ def disconnected():
 def handle_video_url(url):
     print("received video_url: '", str(url).strip(),"'")
     global video_stream, cap
+    global fps
 
     try:
-        video = YouTube(str(url))
-        print(video)
+        video = YouTube(str(url).strip())
         stream = video.streams.get_highest_resolution()
         cap = cv2.VideoCapture(stream.url)
+        fps = stream.fps
+        send_video_info(video, stream)
         send_frames_in_packs(cap)  # Send frames in packs
     except Exception as e:
         socketio.emit("/error", data={"message": f"Error opening video: {e}"})
 
-def send_frames_in_packs(cap):
-    print("Processing video")
+def send_video_info(v, s):
+    ## Collect 
+    video_id = v.video_id
+    title = v.title
+    description = v.description
+    channel_url = v.channel_url
+    channel_name = v.author
+    length = v.length
+    video_fps = s.fps
+    num_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+
+    socketio.emit("/video_info", data={"video_id": video_id, 
+                                        "title": title, 
+                                        "fps": video_fps,
+                                        "description": description, 
+                                        "channel_name": channel_name, 
+                                        "channel_url": channel_url, 
+                                        "num_seconds": length, 
+                                        "num_frames": num_frames})
+
+async def send_frames_in_packs(cap):
+    print("Processing video...")
     frames = []
     frame_count = 0
     while True:
         ret, frame = cap.read()
-        cv2.imshow("video", frame)
-        if not frame:
+        if not ret:
+            if frame_count != 0:
+                socketio.emit("/frame", data=frames)
+
             print("Video Ending... closing connection")
             # End of video, emit a signal to close the socket
             socketio.emit("/video_ended", data={"message": f"Video has completed!"})
             break
-        if not ret:
-            break  # End of video
-
         ret, jpg_buffer = cv2.imencode(".jpg", frame)
         encoded_string = base64.b64encode(jpg_buffer).decode("utf-8")
-        print('sending frame...')
-        socketio.emit("frame", data=encoded_string)
+
+        frames.append(encoded_string)
+        frame_count += 1
+
+        if frame_count == (fps * 2):
+            print('sending frame...')
+            socketio.emit("/frame", data=frames)
+            frames = []
+            frame_count = 0
 
 if __name__ == '__main__':
     socketio.run(app, debug=True,port=5001)
--- gameplay-collection/labelling/frontend/src/App.tsx
+++ gameplay-collection/labelling/frontend/src/App.tsx
@@ -3,11 +3,9 @@ import './App.css';
 import Form from './components/form';
 import { io } from "socket.io-client";
 
-
-
 function App() {
   const [socketInstance, setSocketInstance] = useState(io);
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
   const [buttonStatus, setButtonStatus] = useState(false);
 
   useEffect(() => {
@@ -17,19 +15,28 @@ function App() {
   
     socket.on("/connect", (data: any) => {
       setSocketInstance(socket);
+      setLoading(true);
       console.log("socket connected: ", data);
     });
 
     socket.on("/error", (error: any) => {
+      // TODO: Complete clearing input field and display error message
       console.log("received error message: \n", error["message"]);
     });
 
-    socket.on("frame", (frame) => {
-      console.log(frame)
+    socket.on("/video_info", (info: any) => {
+      console.log(info);
+    })
+
+    socket.on("/frame", (frame: any) => {
+      if (loading) {
+        setLoading(false);
+      }
+      console.log("Received two seconds of frames!");
     });
 
-    socket.on("/video_ended", (data) => {
-      console.log(data)
+    socket.on("/video_ended", (end_message: any) => {
+      console.log(end_message);
     });
 
     return () => {
